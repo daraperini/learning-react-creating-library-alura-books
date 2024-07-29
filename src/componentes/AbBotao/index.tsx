e2a8ccc442e667eccf9dd5dc415befb4d3993325
Dara Lahm Perini <daraperini@gmail.com>
2024-07-29 10:30:35 -0300

    adicionando variantes do AbBotao

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -1,23 +1,45 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-const BotaoEstilizado = styled.button`
-    background: #EB9B00;
-    padding: 16px 32px;
-    border: 2px solid #EB9B00;
-    color: #FFF;
-    font-size: 20px;
-    cursor: pointer;
-    &:hover {
-        background: #B87900;
-        border: 2px solid #B87900;
-    }
-`
-
-export const AbBotao = () => {
-    return (
-        <BotaoEstilizado>
-            Clique aqui!
-        </BotaoEstilizado>
-    )
+export interface AbBotaoProps {
+  texto?: string;
+  tipo?: 'primario' | 'secundario';
+  onClick?: () => void;
 }
+
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+  background: ${(props: AbBotaoProps) =>
+    props.tipo === 'primario' ? '#EB9B00' : '#FFF'};
+  padding: 16px 32px;
+  border: 2px solid #eb9b00;
+  color: ${(props: AbBotaoProps) =>
+    props.tipo === 'primario' ? '#FFF' : '#EB9B00'};
+  font-size: 20px;
+  cursor: pointer;
+  ${(props: AbBotaoProps) =>
+    props.tipo === 'primario'
+      ? css`
+          &:hover {
+            background: #b87900;
+            border: 2px solid #b87900;
+          }
+        `
+      : css`
+          &:hover {
+            border: 2px solid #b87900;
+            color: #b87900;
+          }
+        `}
+`;
+
+export const AbBotao = ({
+  texto,
+  onClick,
+  tipo = 'primario',
+}: AbBotaoProps) => {
+  return (
+    <BotaoEstilizado onClick={onClick} tipo={tipo}>
+      {texto}
+    </BotaoEstilizado>
+  );
+};
